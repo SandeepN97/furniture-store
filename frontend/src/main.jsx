1487b6ce26511bf8576e005531e4e11ed2db990b
Sandeep Nyoupane <65792729+SandeepN97@users.noreply.github.com>
2025-06-16 01:19:43 -0500

    Integrate Stripe checkout

---
+++ frontend/src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import ProductDetails from './ProductDetails';
+import Cart from './Cart';
+import Checkout from './Checkout';
+import OrderHistory from './OrderHistory';
+import Login from './Login';
+import AddProduct from './AddProduct';
+import AdminPanel from './AdminPanel';
+import CheckoutSuccess from './CheckoutSuccess';
+import Layout from './Layout';
+import { CartProvider } from './CartContext';
+import { AuthProvider } from './AuthContext';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<App />} />
+            <Route path="products/:id" element={<ProductDetails />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="orders" element={<OrderHistory />} />
+            <Route path="login" element={<Login />} />
+            <Route path="add-product" element={<AddProduct />} />
+            <Route path="admin" element={<AdminPanel />} />
+            <Route path="success" element={<CheckoutSuccess />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
+  </AuthProvider>
+);
